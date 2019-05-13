285da56d3a5f2944a970e4ac3ed4d607cb29c95a
shaxzad <shaxzad544@gmail.com>
2019-05-13 11:24:27 +0500

    youtube search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,11 +7,18 @@ class SearchBar extends Component {
   }
   render() {
     return (
-      <input
-        value={this.state.term}
-        onChange={event => this.setState({ term: event.target.value })}
-      />
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetails from "./components/video_detail";
 
 const API_KEY = "AIzaSyAgDnH-HNHaTzdgyCdrGVtli8dTQkcLFVw";
 
@@ -9,17 +11,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
-
-    // YTSearch({ key: API_KEY, term: "sufboard" }, videos => {
-    //   this.setState({ videos });
-    // });
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("reactjs");
+  }
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
   }
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={"this.state.videos"} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetails video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
